05f7a75f90894324134a654b4f77d0c485b08568
Ryan Nicholas <ryannicholas.vieira@gmail.com>
2022-08-01 20:10:33 -0300

    Update Modelagem Entidade e Inconsistência_postgres_create.sql

--- Modelagem Entidade e Inconsistência_postgres_create.sql
+++ Modelagem Entidade e Inconsistência_postgres_create.sql
@@ -12,9 +12,10 @@ CREATE TABLE "public.products" (
 	"id" serial NOT NULL,
 	"name" TEXT NOT NULL,
 	"price" double NOT NULL,
-	"image" TEXT NOT NULL,
+	"mainImageId" int NOT NULL,
 	"categoryId" int NOT NULL,
 	"measureId" int NOT NULL,
+	"subImageId" int NOT NULL,
 	CONSTRAINT "products_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -28,7 +29,6 @@ CREATE TABLE "public.categories" (
   OIDS=FALSE
 );
 
-
 CREATE TYPE measure AS ENUM ('P', 'M', 'G');
 CREATE TABLE "public.measures" (
 	"id" serial NOT NULL,
@@ -38,22 +38,19 @@ CREATE TABLE "public.measures" (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public.purchases" (
 	"id" serial NOT NULL,
 	"productId" int NOT NULL,
 	"quantity" int NOT NULL,
 	"price" double NOT NULL,
 	"statusId" int NOT NULL,
-	"deliveryAddress" TEXT NOT NULL,
+	"deliveryAddressId" int NOT NULL,
 	"purchaseTime" DATE NOT NULL,
 	"userId" int NOT NULL,
 	CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
-
 
 CREATE TYPE status AS ENUM ('Criada', 'Paga', 'Entregue','Cancelada');
 CREATE TABLE "public.purchaseStatus" (
@@ -64,17 +61,37 @@ CREATE TABLE "public.purchaseStatus" (
   OIDS=FALSE
 );
 
-ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("categoryId") REFERENCES "categories"("id");
-ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("measureId") REFERENCES "measures"("id");
+CREATE TABLE "public.mainImage" (
+	"id" serial NOT NULL,
+	"image" TEXT NOT NULL,
+	CONSTRAINT "mainImage_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
+CREATE TABLE "public.subImage" (
+	"id" serial NOT NULL,
+	"image" TEXT,
+	CONSTRAINT "subImage_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "public.deliveryAddress" (
+	"id" serial NOT NULL,
+	"address" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	CONSTRAINT "deliveryAddress_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("mainImageId") REFERENCES "mainImage"("id");
+ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("categoryId") REFERENCES "categories"("id");
+ALTER TABLE "products" ADD CONSTRAINT "products_fk2" FOREIGN KEY ("measureId") REFERENCES "measures"("id");
+ALTER TABLE "products" ADD CONSTRAINT "products_fk3" FOREIGN KEY ("subImageId") REFERENCES "subImage"("id");
 ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk0" FOREIGN KEY ("productId") REFERENCES "products"("id");
 ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk1" FOREIGN KEY ("statusId") REFERENCES "purchaseStatus"("id");
-ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk2" FOREIGN KEY ("userId") REFERENCES "users"("id");
-
-
-
-
-
-
-
-
+ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk2" FOREIGN KEY ("deliveryAddressId") REFERENCES "deliveryAddress"("id");
+ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk3" FOREIGN KEY ("userId") REFERENCES "users"("id");
+ALTER TABLE "deliveryAddress" ADD CONSTRAINT "deliveryAddress_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
